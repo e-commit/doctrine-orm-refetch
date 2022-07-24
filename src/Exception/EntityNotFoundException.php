7c6b3336e2cfd8494a8d57e9e9479e9cff72d359
hlecorche <hlecorche@e-commit.fr>
2022-07-24 20:40:46 +0200

    Fix CS

--- src/Exception/EntityNotFoundException.php
+++ src/Exception/EntityNotFoundException.php
@@ -24,7 +24,7 @@ class EntityNotFoundException extends \Exception implements ExceptionInterface
         }
 
         return new self(
-    'Entity of type \''.$className.'\''.($ids ? ' for IDs '.implode(', ', $ids) : '').' was not found'
+            'Entity of type \''.$className.'\''.($ids ? ' for IDs '.implode(', ', $ids) : '').' was not found'
         );
     }
 }
